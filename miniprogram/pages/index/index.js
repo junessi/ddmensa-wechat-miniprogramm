9d12cc14de9a53332e0772a3ef260224bca500a5
Junlin Huang <cricket.long@gmail.com>
2021-04-05 01:41:47 +0200

    a small change

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -143,85 +143,51 @@ Page({
   },
 
   promisify: function(fn) {
-    // promisify() 返回的是一个函数，
-    // 这个函数跟传入的 fn（即 wx.abcd） 签名相同（或兼容）
     return async function(args) {
-    //                    ^^^^ 接受一个单一参数对象
         return new Promise((resolve, reject) => {
-    //             ^^^^^^^^^^^ 返回一个 Promise 对象
             fn({
-    //      ^^ ^ 调用原函数并使用改造过的新的参数对象
                 ...(args || {}),
-    //          ^^^^^^^^        这个新参数对象得有原本传入的参数，
-    //                      ^^  当然得兼容没有传入参数的情况
                 success: res => resolve(res),
-    //          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^  注入 success 回调，resovle 它
                 fail: err => reject(err)
-    //          ^^^^^^^^^^^^^^^^^^^^^^^^ 注入 fail 回调，reject 它
             });
         });
     };
   },
 
   async getLoginCode() {
-    var loginCode = "";
-    try {
-      const promisifiedWxLogin = this.promisify(wx.login);
-      const res = await promisifiedWxLogin();
-      loginCode = res.code;
-    }
-    catch (e) {
-      console.log("wx.login: 获取登录码失败");
+    const promisifiedWxLogin = this.promisify(wx.login);
+    const res = await promisifiedWxLogin();
+    if ("code" in res) {
+      return res.code;
     }
 
-    return loginCode;
-
-    /* before refactor
-    return new Promise((resolve, reject) => {
-      wx.login({
-        success: function(res) {
-          if (res.code) {
-            resolve(res.code);
-          } else {
-            console.log("login: 获取登录码失败");
-            resolve("");
-          }
-        }
-      });
-    });
-    */
+    return "";
   },
 
   async getAppUserInfo(code) {
-    try {
-      const promisifiedWxCheckSession = this.promisify(wx.checkSession);
-      await promisifiedWxCheckSession();
-    }
-    catch (e) {
-      console.log("login: 密钥仍有效，无需重新获取。");
-    }
-
     var appUserInfo = {};
-    try {
-      const promisifiedWxRequest = this.promisify(wx.request);
-      var userInfo = wx.getStorageSync('userInfo');
-      appUserInfo = await promisifiedWxRequest({
-        url: app.globalData.apiBaseUrl + '/wechat/user/login',
-        method: "POST",
-        header: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        data: {
-          "code": code,
-          "rawData": userInfo["rawData"],
-          "signature": userInfo["signature"]
-        }
-      });
 
+    const promisifiedWxCheckSession = this.promisify(wx.checkSession);
+    const ret = await promisifiedWxCheckSession();
+    if (("errMsg" in ret) && (ret["errMsg"] == "checkSession:ok")) {
+      console.log("login: 密钥仍有效，无需重新获取。");
+      return appUserInfo;
     }
-    catch (e) {
-      console.log("Failed to fetch appUserInfo: " + e);
-    }
+
+    const promisifiedWxRequest = this.promisify(wx.request);
+    var userInfo = wx.getStorageSync('userInfo');
+    appUserInfo = await promisifiedWxRequest({
+      url: app.globalData.apiBaseUrl + '/wechat/user/login',
+      method: "POST",
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: {
+        "code": code,
+        "rawData": userInfo["rawData"],
+        "signature": userInfo["signature"]
+      }
+    });
 
     return appUserInfo;
 
@@ -262,19 +228,6 @@ Page({
     userInfo.rawData = res.rawData;
     userInfo.signature = res.signature;
     return userInfo;
-
-    /*
-    return new Promise((resolve, reject) => {
-      wx.getUserInfo({
-        success: function(res) {
-          var userInfo = res.userInfo;
-          userInfo.rawData = res.rawData;
-          userInfo.signature = res.signature;
-          resolve(userInfo);
-        }
-      })
-    });
-    */
   },
 
   async login(e) {
@@ -293,6 +246,8 @@ Page({
     var userId = 0;
     var token = "";
     var userInfo = await this.getUserInfo();
+    console.log("login: userInfo:");
+    console.log(userInfo);
 
     this.setStorage({
       key: 'userInfo',
